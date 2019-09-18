1d8b93edab211637a1e8f4a9150f46e66ce3f534
yaphets <360720285@qq.com>
2019-09-18 20:54:39 +0800

    more edit....

--- src/com/atguigu/egit/test/EgitTest.java
+++ src/com/atguigu/egit/test/EgitTest.java
@@ -4,5 +4,7 @@ public class EgitTest {
 	public static void main(String[] args) {
 		System.out.println("aaa");
 		System.out.println("bbbb");
+		System.out.println("cccc");
+		System.out.println("dddd");
 	}
 }
